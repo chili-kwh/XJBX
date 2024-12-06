04b687b824835c255d87408ef8c30fe06471071a
chili-kwh <714448530@qq.com>
2024-12-06 14:16:34 +0800

    js

---
+++ packages/js/reduce.js
@@ -0,0 +1,10 @@
+/*
+* 1）callbackFn接收4个参数，reduce((pre,cur, index, array) => {})
+pre累加器、cur当前值、 index当前下标、array用于遍历的数组
+2）initialValue作为reduce方法的初始值
+reduce函数内部判断initialValue是否存在，不存在，需要找到数组中第一个存在的值作为初始值
+* */
+
+function reduce(arr) {
+
+}
