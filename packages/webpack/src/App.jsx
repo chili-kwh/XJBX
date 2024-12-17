ef4bf39293f1385dd2939e93fc3ca99e4d53a8c9
chili-kwh <714448530@qq.com>
2024-12-17 11:07:22 +0800

    feat: effect

--- packages/webpack/src/App.jsx
+++ packages/webpack/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Dynamite from "./Dynamite";
+import Effect from "./Effect";
 
 const App = () => {
     return (
         <div>
             App
-            <Dynamite/>
+            <Effect/>
         </div>
     );
 };
--- packages/webpack/src/Effect.jsx
+++ packages/webpack/src/Effect.jsx
@@ -1,7 +1,15 @@
-import React, { useEffect, useState, Suspense } from "react";
+import React, {useLayoutEffect} from "react";
 
 function Effect() {
+    useLayoutEffect(() => {
+        console.log('useLayoutEffect')
+        // while(1){
+        //     // TODO test 阻塞
+        //     // console.log('useLayoutEffect')
+        // }
+    })
 
+    return <div>Effect</div>
 }
 
 export default Effect;
